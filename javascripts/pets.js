aa47647b6eaba41fd78f3fc9962af0352ea1e107
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-12 12:20:03 -0500

    added xhr function

---
+++ javascripts/pets.js
@@ -0,0 +1,9 @@
+// Makes an XHR call
+const xhrPets = (loadFunction, errorFunction) => {
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', loadFunction);
+  myRequest.addEventListener('error', errorFunction);
+  myRequest.open('GET', '../db/dinosaurs.json');
+  myRequest.send();
+};
+module.exports = xhrPets;
