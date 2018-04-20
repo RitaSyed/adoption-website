15832ac4127edf43e54d5971904b68fe27f231a4
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-19 21:24:17 -0500

    added css, project done

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -8,12 +8,12 @@ const domString = (petsArray) => {
   petsArray.forEach((pet) => {
     strang +=  `<div class="pets-cards col-sm-6 col-md-4">`;
     strang +=    `<div class="thumbnail">`;
-    strang +=      `<h2 class="dino">${pet.name}</h2>`;
+    strang +=      `<h2 class="text-center card-title">${pet.name}</h2>`;
     strang +=      `<img src="${pet.imageUrl}" alt="...">`;
     strang +=      `<div class="caption">`;
-    strang +=        `<h3>${pet.color}</h3>`;
-    strang +=        `<p>${pet.specialSkill}</p>`;
-    strang +=        `<p>${pet.type}</p>`;
+    strang +=        `<h3 class="text-center color-title">${pet.color}</h3>`;
+    strang +=        `<p class="skills-info">${pet.specialSkill}</p>`;
+    strang +=        `<p class="text-center pet-type">${pet.type}</p>`;
     strang +=      `</div>`;
     strang +=    `</div>`;
     strang +=  `</div>`;
